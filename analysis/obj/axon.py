876c7eec08b9d907dc59b2cc8cfd26fb00673a15
InbalZelig <inbaladir@gmail.com>
2021-06-21 16:21:38 +0300

    Create basic obj

---
+++ analysis/obj/axon.py
@@ -0,0 +1,14 @@
+from analysis.obj.node import Node
+
+
+class Axon:
+    def __init__(self, id):
+        self.id = id
+        self.nodes = []  # list of nodes in the axon
+        # self.neuron_id = neuron_id  # this is the neuron identifier
+
+    def load_nodes(self, nodes):
+        for node_id in range(0,len(nodes[0])):
+            coordinates = nodes[:, node_id]
+            node = Node(coordinates, node_id)
+            self.nodes.append(node)
---
+++ analysis/obj/dendrite.py
@@ -0,0 +1,5 @@
+class Dendrite:
+    def __init__(self,id):
+        self.id = id
+        self.nodes = []  # list of nodes in the dendrite
+
