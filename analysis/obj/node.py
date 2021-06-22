d2d51f54b92579643389f3e309376b12faa36fe8
Lior Sukman <lior.sukman@gmail.com>
2021-06-22 14:54:04 +0300

    edges

--- analysis/obj/node.py
+++ analysis/obj/node.py
@@ -1,8 +1,8 @@
 class Node:
     def __init__(self, coordinates, node_id):
-        self.x = coordinates[0]
-        self.y = coordinates[1]
-        self.z = coordinates[2]
+        self.x = int(coordinates[0])
+        self.y = int(coordinates[1])
+        self.z = int(coordinates[2])
         self.node_id = node_id  # segment id in the axon/dendrite
 
     def __str__(self):
--- visualization/vis_axons.py
+++ visualization/vis_axons.py
@@ -21,27 +21,19 @@ if __name__ == '__main__':
 
     for key in axons.keys():
         nodes += [[node.x, node.y, node.z] for node in axons[key].nodes]
-        # edges += [[[node1.x, node2.x], [node1.y, node2.y], [node1.z, node2.z]] for node1, node2 in axons[key].edges]
-        edges += [[[node1.x, node1.y, node1.z], [node2.x, node2.y, node2.z]] for node1, node2 in axons[key].edges]
+        edges += [[[node1.x, node1.y, node1.z], [node2.x - node1.x, node2.y - node1.y, node2.z - node1.z]]
+                  for node1, node2 in axons[key].edges]
         values += [np.sqrt(node.x ** 2 + node.y ** 2 + node.z ** 2) for node in axons[key].nodes]
         edge_values += [np.sqrt(node1.x ** 2 + node1.y ** 2 + node1.z ** 2) for node1, _ in axons[key].edges]
         axon_types_nodes += [axons[key].axon_type for _ in axons[key].nodes]
         axon_types_edges += [axons[key].axon_type for _ in axons[key].edges]
 
     nodes = np.array(nodes).astype('int32')
-    edges = np.array([edges[0]]).astype('int32')
-    print(edges.shape)
+    edges = np.array(edges).astype('int32')
     values = np.array(values).astype('int32')
-    edge_values = np.array([edge_values[0]]).astype('int32')
-    print(edge_values.shape)
+    edge_values = np.array(edge_values).astype('int32')
     axon_types_nodes = np.array(axon_types_nodes).astype('int32')
-    axon_types_edges = np.array([axon_types_edges[0]]).astype('int32')
-    print(axon_types_edges.shape)
-
-    """pos = np.zeros(shape=(edges.shape[0], 2, 3), dtype=np.float32) (x,y,z,(x1, y1, z1))
-    pos[:, 0, :] = edges.reshape(edges.shape[0], 3, 2)[:, :, 0]
-    pos[:, 1, :] = edges.reshape(edges.shape[0], 3, 2)[:, :, 1]
-    edges = pos"""
+    axon_types_edges = np.array(axon_types_edges).astype('int32')
 
     viewer = napari.Viewer()
 
@@ -55,6 +47,8 @@ if __name__ == '__main__':
         }
 
         mask_edges = (axon_types_edges == axon_type_tup[0])
+        if mask_edges.sum() == 0:
+            continue
         temp_edges = edges[mask_edges]
         edge_properties_temp = {
             'value': edge_values[mask_edges]
@@ -65,16 +59,13 @@ if __name__ == '__main__':
             properties=point_properties_temp,
             face_color='value',
             size=25,
-            name=axon_type_tup[1]
+            name=f"{axon_type_tup[1]} nodes"
         )
-
-        if mask_edges.sum() == 0:
-            continue
-        print(temp_edges)
 
         _ = viewer.add_vectors(
             temp_edges,
             properties=edge_properties_temp,
             edge_color='value',
-            name=axon_type_tup[1]
+            edge_width=4,
+            name=f"{axon_type_tup[1]} edges"
         )
