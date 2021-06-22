f212d61b44f65831f535950a4400cbaba1eb7e50
AdiSarig <74588101+AdiSarig@users.noreply.github.com>
2021-06-22 18:25:29 +0300

    Down sampling

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -79,6 +79,7 @@ def load_blood_vessels():
 
         # Add new blood vessels coordinates to the matrix
         print('Appending new blood vessel indices to the matrix')
+        blood_vessels = np.concatenate((blood_vessels, coordinates[0::100,]))
 
     return blood_vessels
 
