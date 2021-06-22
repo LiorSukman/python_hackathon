859da19f0319282ef0c76a8fbc72b3cd439b06c5
InbalZelig <inbaladir@gmail.com>
2021-06-22 15:57:33 +0300

    Calc distances for each node to all blood vessels and find min dist for the node

--- analysis/obj/node.py
+++ analysis/obj/node.py
@@ -4,3 +4,13 @@ class Node:
         self.y = coordinates[1]
         self.z = coordinates[2]
         self.node_id = node_id  # segment id in the axon/dendrite
+        self.distance = float('inf')
+
+    def update_distance_from_blood_vessel(self, dist):
+        if dist < self.distance:
+            self.distance = dist
+
+    def convert_coordinates_to_int(self):
+        self.x = int(self.x)
+        self.y = int(self.y)
+        self.z = int(self.z)
