fff0ed3e738e162fd7ec347651f22904639a001f
InbalZelig <inbaladir@gmail.com>
2021-06-22 20:42:05 +0300

    Update fetch_data.py

--- conf.py
+++ conf.py
@@ -1,6 +1,12 @@
+AXONS_URL = 'https://l4dense2019.rzg.mpg.de/webdav/axons.hdf5'
+DENDRITES_URL = 'https://l4dense2019.rzg.mpg.de/webdav/dendrites.hdf5'
+BLOOD_VESSEL_SEG_URL = 'https://l4dense2019.rzg.mpg.de/webdav/blood-vessel-segmentation-volume/'
+
 DATA_BASE_PATH = '../data'
 AXONS_FILE_PATH = f'{DATA_BASE_PATH}/axons.hdf5'
 DENDRITES_FILE_PATH = f'{DATA_BASE_PATH}/dendrites.hdf5'
+BLOOD_VESSEL_BOXES_BASE_PATH = f'{DATA_BASE_PATH}/blood-vessel-segmentation'
+
 BLOOD_VESSEL_BOXES = [
     'x0y0z1.hdf5', 'x0y1z1.hdf5', 'x0y2z0.hdf5', 'x0y2z3.hdf5', 'x0y3z0.hdf5', 'x0y3z2.hdf5', 'x0y3z3.hdf5',
     'x0y4z0.hdf5', 'x0y4z2.hdf5', 'x0y4z3.hdf5', 'x0y5z0.hdf5', 'x0y5z1.hdf5', 'x0y5z2.hdf5', 'x0y5z3.hdf5',
@@ -16,6 +22,6 @@ BLOOD_VESSEL_BOXES = [
     'x4y6z2.hdf5', 'x4y7z1.hdf5', 'x4y7z2.hdf5', 'x5y0z1.hdf5', 'x5y0z2.hdf5', 'x5y1z1.hdf5', 'x5y1z2.hdf5',
     'x5y2z2.hdf5', 'x5y4z2.hdf5', 'x5y4z3.hdf5', 'x5y5z2.hdf5', 'x5y5z3.hdf5', 'x5y6z2.hdf5'
 ]
-BLOOD_VESSEL_BOXES_BASE_PATH = f'{DATA_BASE_PATH}/blood-vessel-segmentation'
 
-NUMBER_OF_PARTS_TO_ANALYZE = 5
+
+NUMBER_OF_PARTS_TO_ANALYZE = 2
--- data/fetch_data.py
+++ data/fetch_data.py
@@ -4,36 +4,37 @@ import os
 import wget
 from directory_downloader import DDownloader
 
-from conf import BLOOD_VESSEL_BOXES_BASE_PATH
-
-# Change this parameters according to your needs
-URL = "https://l4dense2019.rzg.mpg.de/webdav/blood-vessel-segmentation-volume/"
-DEST_FOLDER = BLOOD_VESSEL_BOXES_BASE_PATH
-FILE_EXTENSIONS = ['.hdf5']
+from conf import *
 
 
-async def main():
+async def download_whole_dir(url, dest_folder, file_extensions):
     """
     Download files from URL.
-    If you get an error like "Couldn't find a tree builder with the features you requested: lxml. Do you need to install a parser library?"
+    If you get an error like "Couldn't find a tree builder with the features you requested: lxml.
+    Do you need to install a parser library?"
     Try to install this:
         - pip install bs4
         - pip install html5lib
         - pip install lxml
     And restart your IDE. The problem should be solved.
     """
-    downloader = DDownloader(URL)
-    await downloader.fetch_file_links(extensions=FILE_EXTENSIONS)  # returns set of downloadable file urls
+    downloader = DDownloader(url)
+    await downloader.fetch_file_links(extensions=file_extensions)  # returns set of downloadable file urls
     links = downloader.files_urls
-    os.makedirs(DEST_FOLDER, exist_ok=True)
+    os.makedirs(dest_folder, exist_ok=True)
     for link in links:
         if link.find('/'):
             file_name = link.rsplit('/', 1)[1]
-            output = f'{DEST_FOLDER}/{file_name}'
+            output = f'{dest_folder}/{file_name}'
             if not os.path.isfile(output):
                 print(f'downloading file {file_name}')
                 wget.download(link, out=output)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    # Download whole dir of files from blood-vessel-segmentation
+    asyncio.run(download_whole_dir(BLOOD_VESSEL_SEG_URL, BLOOD_VESSEL_BOXES_BASE_PATH, ['.hdf5']))
+
+    # download axons.hdf5 and dendrites.hdf5
+    wget.download(AXONS_URL, out=AXONS_FILE_PATH)
+    wget.download(DENDRITES_URL, out=DENDRITES_FILE_PATH)
